2bb240b6f6037f27aba3cb51c7e05158bad8c62f
ell1jah <ilya.grinkewitch@yandex.ru>
2023-11-22 13:05:56 +0300

    Removed trash

--- docs/inc/lst/index.sql
+++
@@ -1,2 +0,0 @@
-CREATE CLUSTERED INDEX user_id_index ON Orders (UserId);
-CREATE NONCLUSTERED INDEX user_id_index ON Orders (UserId);
--- docs/inc/lst/user.go
+++
@@ -1,8 +0,0 @@
-type User struct {
-	ID       int
-	Login    string
-	Password string
-	Name     string
-	Sex      string
-	Role     string
-}
